b14a2394130868a53e5da04f7ce80a6795d6022f
Lucas Brasilino <lucas.brasilino@gmail.com>
2013-09-17 09:16:47 -0300

    Initial import

---
+++ reference/wrr.py
@@ -0,0 +1,30 @@
+from sets import Set
+
+# http://kb.linuxvirtualserver.org/wiki/Weighted_Round-Robin_Scheduling
+
+def schedule(cw,gcd,maxi,index,queues):
+  while True:
+    index=(index+1) % len(queues)
+    if index == 0:
+      cw = cw - gcd
+      if cw <= 0:
+        cw = maxi
+        if cw == 0:
+          return None
+    if queues[index][1] >= cw:
+      return (queues[index][0],index,cw)
+
+index = -1
+cw = 0;
+queues = [("MAC3",8),("MAC2",4),("MAC1",2),("MAC0",1)]
+results = []
+maxi = 8;
+gcd=1
+
+for i in range (0,1000):
+  (result,index,cw) = schedule(cw,gcd,maxi,index,queues)
+  results = results + [result]
+
+unique_set = Set(results)
+for res in unique_set:
+  print "%s: serviced %s times " % (res,results.count(res))
